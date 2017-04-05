b83fb8bcf9319334a2a98f7ee1a25342bce96bda
Raphael Odini <raphael.odini@ekimetrics.com>
2017-04-05 02:18:36 +0200

    add list of skills level_up in pokemon details

--- src/app/pokemon-card/pokemon-card.component.ts
+++ src/app/pokemon-card/pokemon-card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { PokemonListComponent } from '../pokemon-list/pokemon-list.component';
 
 @Component({
   selector: 'app-pokemon-card',
@@ -7,11 +8,24 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PokemonCardComponent implements OnInit {
 
-  constructor() { }
+  // constructor() { }
+
+  // inject the Parent Component to get access to the pokemonSkills list (not great but only way I found)
+  constructor(private pokemonListComponent: PokemonListComponent) { }
+  //   console.log(pokemonListComponent.pokemonSkills);
+  //   // this.pokemonSkills = PokemonListComponent.pokemonSkills;
+  // }
 
   @Input() pokemon: any;
 
   ngOnInit() {
+  }
+
+  getSkill(skillId) {
+    return this.pokemonListComponent.pokemonSkills.find(function(skill) {
+      return (skill.id == skillId);
+    }).ename;
+
   }
 
 }
